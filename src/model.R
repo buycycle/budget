525186bfd457b7b305fac17638a311f6818b0ab1
rauner <29773634+rauner@users.noreply.github.com>
2025-01-08 09:49:34 +0100

    read columns from snowflake, filter for country, add timewindow

--- src/model.R
+++ src/model.R
@@ -26,45 +26,40 @@ library(reticulate)
 ############# START ROBYN MODEL ################
 ################################################
 
-data("dt_prophet_holidays")
-head(dt_prophet_holidays)
-
 library(dplyr)
-# Create a mock data frame with 10 rows
-mock_length = 200
-df <- tibble(
-  date = seq.Date(from = as.Date("2024-01-01"), by = "days", length.out = mock_length),
-  revenue = runif(mock_length, 1000, 5000),  # Random revenue values between 1000 and 5000
-  trend = runif(mock_length, 0, 1),
-  season = runif(mock_length, 0, 1),
-  weekday = sample(0:6, mock_length, replace = TRUE),  # Random weekdays
-  uploads_private = runif(mock_length, 50, 200),
-  uploads_commercial = runif(mock_length, 30, 150),
-  google_ads_brand_spend = runif(mock_length, 30, 150),
-  organic_google = runif(mock_length, 30, 150),
-)
+
+# read csv snowflake export
+data_path <- "data/data.csv"
+df <- read.csv(data_path)
+
+country_code <- "FR"
+df_filtered <- df %>% filter(country == country_code)
+
+# Determine the minimum and maximum dates
+window_start <- min(df_filtered$date, na.rm = TRUE)
+window_end <- max(df_filtered$date, na.rm = TRUE)
+print(window_start)
+print(window_end)
+
 
 inputcollect <- robyn_inputs(
-  dt_input = df,
+  dt_input = df_filtered,
   dt_holidays = dt_prophet_holidays,
 
   date_var = "date", # date format must be "2020-01-01"
-  dep_var = "revenue", # there should be only one dependent variable
+  dep_var = "gmv", # there should be only one dependent variable
   dep_var_type = "revenue", # "revenue" (roi) or "conversion" (cpa)
 
   prophet_vars = c("trend","season", "weekday"),  #"trend","season", "weekday" & "holiday"
   #prophet_country = "de", # input one country. dt_prophet_holidays includes 59 countries by default
 
-  context_vars = c("uploads_private","uploads_commercial"),
-
-  paid_media_spends =  c("google_ads_brand_spend"),
-  paid_media_vars =  c("google_ads_brand_spend"),
-  # paid_media_vars must have same order as paid_media_spends. use media exposure metrics like
-  # impressions, grp etc. if not applicable, use spend instead.
-  organic_vars = c("organic_google"), # marketing activity without media spend
+  context_vars = c("uploads_private", "uploads_commercial", "crossborder_sales", "n_distinct_searches", "app_installs"), # e.g. competitors, discount, unemployment etc
+  paid_media_spends = c("ga_brand_search_spend", "ga_demand_search_spend", "ga_demand_pmax_spend", "ga_demand_shopping_spend", "ga_supply_search_spend", "ga_supply_pmax_spend", "meta_brand_spend", "meta_supply_spend", "meta_demand_spend", "tv_spent_eur", "ga_app_spend", "youtube_spend", "youtube_spend", "google_ads_dg", "google_ads_dg", "google_ads_dg", "youtube_spend"), # mandatory input
+  paid_media_vars = c("ga_brand_search_spend", "ga_demand_search_spend", "ga_demand_pmax_spend", "ga_demand_shopping_spend", "ga_supply_search_spend", "ga_supply_pmax_spend", "meta_brand_spend", "meta_supply_spend", "meta_demand_spend", "tv_spent_eur", "ga_app_spend", "youtube_spend", "youtube_spend", "google_ads_dg", "google_ads_dg", "google_ads_dg", "youtube_spend"), # mandatory.
+  organic_vars = c("organic_google", "blog_traffic", "referral"), # marketing activity without media spend
   #factor_vars = c("m_tdf"), # force variables in context_vars or organic_vars to be categorical
-  window_start = "2024-01-01",
-  window_end = "2024-02-05",
+  window_start = window_start,
+  window_end = window_end,
   adstock = "weibull_pdf" # geometric, weibull_cdf or weibull_pdf.
 )
 OutputCollect <- robyn_run(
