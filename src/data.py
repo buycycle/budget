7fabe200d6097a4964989a5291d41262884eeb7a
rauner <29773634+rauner@users.noreply.github.com>
2025-01-09 13:28:43 +0000

    use full dataset

--- src/data.py
+++ src/data.py
@@ -1,14 +1,10 @@
 import pandas as pd
 from buycycle.data import snowflake_sql_db_read
-from datetime import datetime
-timeframe= datetime.now() - pd.DateOffset(months=6)
-timeframe= timeframe.strftime('%Y-%m-%d')
 # Define the query to fetch filtered data
 query = f"""
 SELECT *
 FROM dwh.bl.report_mmm_all
-WHERE date >= '{timeframe}'
-AND management_region= 'DACH'
+WHERE management_region= 'DACH'
 AND country= 'DE'
 ORDER BY date ASC
 """
