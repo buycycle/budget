624b46613626ebf6b564684ba30e75f3c4bfd713
rauner <29773634+rauner@users.noreply.github.com>
2025-01-09 08:24:15 +0000

    remove date mock

--- src/model.R
+++ src/model.R
@@ -34,21 +34,14 @@ df <- read.csv(data_path)
 
 df$date = as.Date(df$date,  format = "%Y-%m-%d")
 df <- df[order(df$date),]
-df$date_mock <- seq.Date(from = as.Date("2024-01-01"),
-                    by = "day",
-                    length.out = nrow(df))
 
 
 
-country_code <- "FR"
-df_filtered <- df %>% filter(country == country_code) %>% select(-country, -management_region)
-
-abs.Date <- function(x){x}
 
 inputcollect <- robyn_inputs(
-  dt_input = df_filtered,
+  dt_input = df,
   dt_holidays = dt_prophet_holidays,
-  date_var = "date_mock",
+  date_var = "date",
 
   dep_var = "gmv", # there should be only one dependent variable
   dep_var_type = "revenue", # "revenue" (roi) or "conversion" (cpa)
@@ -61,8 +54,8 @@ inputcollect <- robyn_inputs(
   paid_media_vars = c("ga_brand_search_spend", "ga_demand_search_spend", "ga_demand_pmax_spend", "ga_demand_shopping_spend", "ga_supply_search_spend", "ga_supply_pmax_spend", "meta_brand_spend", "meta_supply_spend", "meta_demand_spend", "tv_spent_eur", "ga_app_spend", "youtube_spend", "google_ads_dg"),
   organic_vars = c("organic_google", "blog_traffic", "referral"),
   factor_vars = c("tv_is_on"), # force variables in context_vars or organic_vars to be categorical
-  window_start = "2024-01-01",
-  window_end = "2024-02-05",
+  window_start = "2024-05-01",
+  window_end = "2024-12-01",
   adstock = "weibull_pdf" # geometric, weibull_cdf or weibull_pdf.
 )
 OutputCollect <- robyn_run(
