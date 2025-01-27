d0cdfc24edae40e9fc45e1b6bc8561fcff7d4c6f
rauner <29773634+rauner@users.noreply.github.com>
2025-01-27 13:46:57 +0000

    add region as parameter, add hyperparameters

--- src/data.py
+++ src/data.py
@@ -1,17 +1,21 @@
+import sys
 import pandas as pd
 from buycycle.data import snowflake_sql_db_read
-# Define the query to fetch filtered data
-query = f"""
-SELECT *
-FROM dwh.bl.report_mmm_all
-WHERE management_region= 'DACH'
-AND country= 'DE'
-ORDER BY date ASC
-"""
-# Execute the query and read the data into a DataFrame
-df = snowflake_sql_db_read(query=query, DB="DB", driver="snowflake")
-# Fill NaN values with 0
-df = df.fillna(0)
-# Save the filtered DataFrame to a CSV file
-df.to_csv("data/data.csv", index=False)
+def fetch_and_save_data(country, management_region, db_name="DB", output_file="data/data.csv"):
+    query = f"""
+    SELECT *
+    FROM dwh.bl.report_mmm_all
+    WHERE management_region = '{management_region}'
+    AND country = '{country}'
+    ORDER BY date ASC
+    """
+    df = snowflake_sql_db_read(query=query, DB=db_name, driver="snowflake")
+    df = df.fillna(0)
+    df.to_csv(output_file, index=False)
+if __name__ == "__main__":
+    # Get arguments from command line
+    country = sys.argv[1]
+    management_region = sys.argv[2]
+    fetch_and_save_data(country, management_region)
+
 
--- src/model.R
+++ src/model.R
@@ -29,12 +29,61 @@ library(reticulate)
 
 source("src/data.R")
 
+country <- "DE"
+management_region <- "DACH"
+# Construct the command to call the Python script
+fetch_data <- sprintf("python src/data.py %s %s", country, management_region)
+# Execute the command
+system(fetch_data)
+
 # read csv snowflake export
 data_path <- "data/data.csv"
 df <- read.csv(data_path)
 
 df <- fill_missing_days(df)
 
+hyperparameters <- list(
+  ga_brand_search_spend_alphas = c(0.5, 3),
+  ga_brand_search_spend_gammas = c(0.3, 1),
+  ga_brand_search_spend_scales = c(0, 0.1),
+  ga_brand_search_spend_shapes = c(0.0001, 10),
+  ga_demand_pmax_spend_alphas = c(0.5, 3),
+  ga_demand_pmax_spend_gammas = c(0.3, 1),
+  ga_demand_pmax_spend_scales = c(0, 0.1),
+  ga_demand_pmax_spend_shapes = c(0.0001, 10),
+  ga_demand_search_spend_alphas = c(0.5, 3),
+  ga_demand_search_spend_gammas = c(0.3, 1),
+  ga_demand_search_spend_scales = c(0, 0.1),
+  ga_demand_search_spend_shapes = c(0.0001, 10),
+  ga_demand_shopping_spend_alphas = c(0.5, 3),
+  ga_demand_shopping_spend_gammas = c(0.3, 1),
+  ga_demand_shopping_spend_scales = c(0, 0.1),
+  ga_demand_shopping_spend_shapes = c(0.0001, 10),
+  ga_supply_search_spend_alphas = c(0.5, 3),
+  ga_supply_search_spend_gammas = c(0.3, 1),
+  ga_supply_search_spend_scales = c(0, 0.1),
+  ga_supply_search_spend_shapes = c(0.0001, 10),
+  google_ads_dg_alphas = c(0.5, 3),
+  google_ads_dg_gammas = c(0.3, 1),
+  google_ads_dg_scales = c(0, 0.1),
+  google_ads_dg_shapes = c(0.0001, 10),
+  meta_brand_spend_alphas = c(0.5, 3),
+  meta_brand_spend_gammas = c(0.3, 1),
+  meta_brand_spend_scales = c(0, 0.1),
+  meta_brand_spend_shapes = c(0.0001, 10),
+  meta_demand_spend_alphas = c(0.5, 3),
+  meta_demand_spend_gammas = c(0.3, 1),
+  meta_demand_spend_scales = c(0, 0.1),
+  meta_demand_spend_shapes = c(0.0001, 10),
+  meta_supply_spend_alphas = c(0.5, 3),
+  meta_supply_spend_gammas = c(0.3, 1),
+  meta_supply_spend_scales = c(0, 0.1),
+  meta_supply_spend_shapes = c(0.0001, 10),
+  tv_spent_eur_alphas = c(0.5, 3),
+  tv_spent_eur_gammas = c(0.3, 1),
+  tv_spent_eur_scales = c(0, 0.1),
+  tv_spent_eur_shapes = c(0.0001, 10)
+)
 
 
 
@@ -56,173 +105,17 @@ InputCollect <- robyn_inputs(
 
   organic_vars = c(),
   factor_vars = c("tv_is_on"), # force variables in context_vars or organic_vars to be categorical
+  hyperparameters=hyperparameters,
   adstock = "weibull_pdf" # geometric, weibull_cdf or weibull_pdf.
 )
 
 hyper_names(adstock = InputCollect$adstock, all_media = InputCollect$all_media)
 
-hyperparameters <- list(
-  #App_Installs_alphas = c(0.5, 3),
-  #App_Installs_gammas = c(0.3, 1),
-  #App_Installs_thetas = c(0.3, 0.8),
-  #backlinks_alphas = c(0.5, 3),
-  #backlinks_gammas= c(0.3, 1),
-  #backlinks_thetas= c(0, 0.3),
-  #biketest_referral_alphas = c(0.5, 3),
-  #biketest_referral_gammas= c(0.3, 1),
-  #biketest_referral_thetas= c(0, 0.3),
-  #bing_alphas= c(0.5, 3),
-  #bing_gammas= c(0.3, 1),
-  #bing_thetas= c(0, 0.3),
-  #ecosia.org_referral_alphas= c(0.5, 3),
-  #ecosia.org_referral_gammas= c(0.3, 1),
-  #ecosia.org_referral_thetas= c(0, 0.3),
-  #eurosport_traffic_alphas= c(0.5, 3),
-  #eurosport_traffic_gammas= c(0.3, 1),
-  #eurosport_traffic_thetas= c(0, 0.3),
-  google_ads_brand_spend_alphas= c(0.5, 3),
-  google_ads_brand_spend_gammas= c(0.3, 1),
-  #google_ads_brand_spend_thetas= c(0, 0.9),
-  google_ads_brand_spend_shapes = c(0.0001, 10),
-  google_ads_brand_spend_scales = c(0, 0.1),
-
-  google_ads_demandsearch_spend_alphas= c(0.5, 3),
-  google_ads_demandsearch_spend_gammas= c(0.3, 1),
-  google_ads_demandsearch_spend_shapes = c(0.0001, 10),
-  google_ads_demandsearch_spend_scales = c(0, 0.1),
-
-  google_ads_demandpmax_spend_alphas= c(0.5, 3),
-  google_ads_demandpmax_spend_gammas= c(0.3, 1),
-  google_ads_demandpmax_spend_shapes = c(0.0001, 10),
-  google_ads_demandpmax_spend_scales = c(0, 0.1),
-
-  google_ads_demandshoppingproduct_spend_alphas= c(0.5, 3),
-  google_ads_demandshoppingproduct_spend_gammas= c(0.3, 1),
-  google_ads_demandshoppingproduct_spend_shapes = c(0.0001, 10),
-  google_ads_demandshoppingproduct_spend_scales = c(0, 0.1),
-
-  #google_ads_demand_spend_thetas= c(0, 0.9),
-  google_ads_supplysearch_spend_alphas= c(0.5, 3),
-  google_ads_supplysearch_spend_gammas= c(0.3, 1),
-  #google_ads_supply_spend_thetas= c(0, 0.9),
-  google_ads_supplysearch_spend_shapes = c(0.0001, 10),
-  google_ads_supplysearch_spend_scales = c(0, 0.1),
-
-  google_ads_supplypmax_spend_alphas= c(0.5, 3),
-  google_ads_supplypmax_spend_gammas= c(0.3, 1),
-  google_ads_supplypmax_spend_shapes = c(0.0001, 10),
-  google_ads_supplypmax_spend_scales = c(0, 0.1),
-
-  #google_ads_supplypmaxCAL_spend_alphas= c(0.5, 3),
-  #google_ads_supplypmaxCAL_spend_gammas= c(0.3, 1),
-  #google_ads_supplypmaxCAL_spend_shapes = c(0.0001, 10),
-  #google_ads_supplypmaxCAL_spend_scales = c(0, 0.1),
-
-  #linkinbio_referral_alphas= c(0.5, 3),
-  #linkinbio_referral_gammas= c(0.3, 1),
-  #linkinbio_referral_thetas= c(0.1, 0.4),
-  meta_ads_brand_spend_alphas= c(0.5, 3),
-  meta_ads_brand_spend_gammas= c(0.3, 1),
-  meta_ads_brand_spend_shapes = c(0.0001, 10),
-  meta_ads_brand_spend_scales = c(0, 0.1),
-
-  meta_ads_supply_spend_alphas= c(0.5, 3),
-  meta_ads_supply_spend_gammas= c(0.3, 1),
-  meta_ads_supply_spend_shapes = c(0.0001, 10),
-  meta_ads_supply_spend_scales = c(0, 0.1),
-
-  meta_ads_demand_spend_alphas= c(0.5, 3),
-  meta_ads_demand_spend_gammas= c(0.3, 1),
-  meta_ads_demand_spend_shapes = c(0.0001, 10),
-  meta_ads_demand_spend_scales = c(0, 0.1),
-
-  #meta_supply_spend_alphas= c(0.5, 3),
-  #meta_supply_spend_gammas= c(0.3, 1),
-  #meta_supply_spend_thetas= c(0, 0.3),
-  #newsletter_alphas= c(0.5, 3),
-  #newsletter_gammas= c(0.3, 1),
-  #newsletter_thetas= c(0.1, 0.4),
-  #ninetyninespokes_alphas= c(0.5, 3),
-  #ninetyninespokes_gammas= c(0.3, 1),
-  #ninetyninespokes_thetas= c(0, 0.3),
-  organic_google_alphas= c(0.5, 3),
-  organic_google_gammas= c(0.3, 1),
-  #organic_google_thetas= c(0, 0.9),
-  organic_google_shapes = c(0.0001, 10),
-  organic_google_scales = c(0, 0.1),
-
-  #bing_alphas= c(0.5, 3),
-  #bing_gammas= c(0.3, 1),
-  #bing_thetas= c(0, 0.9),
-  #bing_shapes = c(0.0001, 10),
-  #bing_scales = c(0, 0.1),
-
-  blog_traffic_alphas= c(0.5, 3),
-  blog_traffic_gammas= c(0.3, 1),
-  #blog_traffic_thetas= c(0, 0.9),
-  blog_traffic_shapes = c(0.0001, 10),
-  blog_traffic_scales = c(0, 0.1),
-  #partnerhsips_alphas= c(0.5, 3),
-  #partnerhsips_gammas= c(0.3, 1),
-  #partnerhsips_thetas= c(0.1, 0.4),
-  referral_alphas= c(0.5, 3),
-  referral_gammas= c(0.3, 1),
-  #referral_thetas= c(0.1, 0.9),
-  referral_shapes = c(0.0001, 10),
-  referral_scales = c(0, 0.1),
-  TV_Ad_spend_alphas= c(0.5, 3),
-  TV_Ad_spend_gammas= c(0.3, 1),
-  #referral_thetas= c(0.1, 0.9),
-  TV_Ad_spend_shapes = c(0.0001, 10),
-  TV_Ad_spend_scales = c(0, 0.1),
-  google_ads_app_alphas= c(0.5, 3),
-  google_ads_app_gammas= c(0.3, 1),
-  #google_ads_supply_spend_thetas= c(0, 0.9),
-  google_ads_app_shapes = c(0.0001, 10),
-  google_ads_app_scales = c(0, 0.1),
-
-  google_ads_YT_prosp_alphas= c(0.5, 3),
-  google_ads_YT_prosp_gammas= c(0.3, 1),
-  #google_ads_supply_spend_thetas= c(0, 0.9),
-  google_ads_YT_prosp_shapes = c(0.0001, 10),
-  google_ads_YT_prosp_scales = c(0, 0.1),
-
-  google_ads_YT_ret_alphas= c(0.5, 3),
-  google_ads_YT_ret_gammas= c(0.3, 1),
-  #google_ads_supply_spend_thetas= c(0, 0.9),
-  google_ads_YT_ret_shapes = c(0.0001, 10),
-  google_ads_YT_ret_scales = c(0, 0.1),
-
-  google_ads_DG_brand_alphas= c(0.5, 3),
-  google_ads_DG_brand_gammas= c(0.3, 1),
-  #google_ads_supply_spend_thetas= c(0, 0.9),
-  google_ads_DG_brand_shapes = c(0.0001, 10),
-  google_ads_DG_brand_scales = c(0, 0.1),
-
-  google_ads_DG_supply_alphas= c(0.5, 3),
-  google_ads_DG_supply_gammas= c(0.3, 1),
-  #google_ads_supply_spend_thetas= c(0, 0.9),
-  google_ads_DG_supply_shapes = c(0.0001, 10),
-  google_ads_DG_supply_scales = c(0, 0.1),
-
-  google_ads_DG_demand_alphas= c(0.5, 3),
-  google_ads_DG_demand_gammas= c(0.3, 1),
-  #google_ads_supply_spend_thetas= c(0, 0.9),
-  google_ads_DG_demand_shapes = c(0.0001, 10),
-  google_ads_DG_demand_scales = c(0, 0.1),
-
-  YT_Flobikes_alphas= c(0.5, 3),
-  YT_Flobikes_gammas= c(0.3, 1),
-  #google_ads_supply_spend_thetas= c(0, 0.9),
-  YT_Flobikes_shapes = c(0.0001, 10),
-  YT_Flobikes_scales = c(0, 0.1)
-)
-
-InputCollect <- robyn_inputs(InputCollect = InputCollect, hyperparameters = hyperparameters)
+InputCollect <- robyn_inputs(InputCollect = InputCollect)
 
 
 OutputCollect <- robyn_run(
-  InputCollect = inputcollect,
+  InputCollect = InputCollect,
   cores = 4, # Number of CPU cores to use
   iterations = 2000, # Number of iterations for the model
   trials = 5 # Number of trials for hyperparameter optimization
