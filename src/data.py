f2172ba4afa3aa6fe36c726d3da9eab4f705a598
rauner <29773634+rauner@users.noreply.github.com>
2025-02-18 12:22:47 +0000

    add targets and campaigne level data output

--- src/data.py
+++ src/data.py
@@ -1,10 +1,10 @@
 import sys
 import pandas as pd
 from buycycle.data import snowflake_sql_db_read
-def fetch_and_save_data(country, management_region, db_name="DB", output_file="data/data.csv"):
+def fetch_and_save_data(country, management_region, table_name, db_name="DB", output_file="data/data.csv"):
     query = f"""
     SELECT *
-    FROM dwh.bl.report_mmm_all
+    FROM {table_name}
     WHERE management_region = '{management_region}'
     AND country = '{country}'
     ORDER BY date ASC
@@ -16,6 +16,19 @@ if __name__ == "__main__":
     # Get arguments from command line
     country = sys.argv[1]
     management_region = sys.argv[2]
-    fetch_and_save_data(country, management_region)
+    fetch_and_save_data(country=country,
+                        management_region=management_region,
+                        table_name="dwh.bl.report_mmm_all",
+                        output_file="data/data.csv")
 
 
+    fetch_and_save_data(country=country,
+                        management_region=management_region,
+                        table_name="dwh.bl.report_mmm_all2",
+                        output_file="data/data_campaigne.csv")
+
+    fetch_and_save_data(country=country,
+                        management_region=management_region,
+                        db_name="DB_TARGET",
+                        table_name="dwh.il.dim_growth_targets",
+                        output_file="data/data_target.csv")
