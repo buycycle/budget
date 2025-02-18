ed14284eba6d9a026ba21ea8e97fc8e6f2c497a3
rauner <29773634+rauner@users.noreply.github.com>
2025-02-18 14:07:35 +0000

    add target read in

--- src/data.py
+++ src/data.py
@@ -12,6 +12,15 @@ def fetch_and_save_data(country, management_region, table_name, db_name="DB", ou
     df = snowflake_sql_db_read(query=query, DB=db_name, driver="snowflake")
     df = df.fillna(0)
     df.to_csv(output_file, index=False)
+def fetch_and_save_target(management_region, table_name, db_name="DB", output_file="data/data.csv"):
+    query = f"""
+    SELECT *
+    FROM {table_name}
+    WHERE management_region = '{management_region}'
+    """
+    df = snowflake_sql_db_read(query=query, DB=db_name, driver="snowflake")
+    df = df.fillna(0)
+    df.to_csv(output_file, index=False)
 if __name__ == "__main__":
     # Get arguments from command line
     country = sys.argv[1]
@@ -27,8 +36,7 @@ if __name__ == "__main__":
                         table_name="dwh.bl.report_mmm_all2",
                         output_file="data/data_campaigne.csv")
 
-    fetch_and_save_data(country=country,
-                        management_region=management_region,
+    fetch_and_save_target(management_region=management_region,
                         db_name="DB_TARGET",
                         table_name="dwh.il.dim_growth_targets",
                         output_file="data/data_target.csv")
